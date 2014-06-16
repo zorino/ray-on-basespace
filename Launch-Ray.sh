3d0b95eaa8b9991fb37d85f476768b23698b9cbe
Maxime Déraspe <maximilien1er@gmail.com>
2014-06-16 13:48:30 +0000

    Launch-Ray.sh Ray will write directly to the output directory
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- Launch-Ray.sh
+++ Launch-Ray.sh
@@ -8,14 +8,13 @@ cd /opt/
 kmersize=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json kmersize)
 projectID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json projectID)
 sampleID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json sampleID)
-mkdir Search-Datasets
 reads=$(find /data/input/samples/$sampleID -name "*fastq*")
 readDir=$(dirname $reads)
-bash /opt/ray-on-basespace/Generate-RayConf.sh -r $readDir -d ./Search-Datasets -k $kmersize -o 
-
+mkdir Search-Datasets
+mkdir -p /data/output/appresults/$projectID/
+bash /opt/ray-on-basespace/Generate-RayConf.sh -r $readDir -d ./Search-Datasets -k $kmersize -o /data/output/appresults/$projectID/$sampleID
 echo "Running Ray Assembly.."
-mpiexec -n 32 ./ray/BUILD/Ray Ray.conf
-mkdir -p /data/output/appresults/$projectID/$sampleID
-mv Assembly/* /data/output/appresults/$projectID/$sampleID/
+mpiexec -n 32 /opt/ray/BUILD/Ray Ray.conf
+ls /data/output/appresults/$projectID/$sampleID
 
 echo "Assembly Finished"
