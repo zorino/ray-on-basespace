a5a70eff2afdfac50c9912caca63c46905f70f9e
Maxime Déraspe <maximilien1er@gmail.com>
2014-06-18 16:48:05 +0000

    Launch-Ray.sh fixed for finding read dir
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- Launch-Ray.sh
+++ Launch-Ray.sh
@@ -8,7 +8,7 @@ cd /opt/
 kmersize=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json kmersize)
 projectID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json projectID)
 sampleID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json sampleID)
-reads=$(find /data/input/samples/$sampleID -name "*fastq*")
+reads=$(find /data/input/samples/$sampleID -name "*fastq*" | head -n 1)
 readDir=$(dirname $reads)
 mkdir Search-Datasets
 mkdir -p /data/output/appresults/$projectID/
