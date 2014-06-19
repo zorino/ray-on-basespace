12fe71e4453af09e288aa92e56e5b311325b1294
Maxime Déraspe <maximilien1er@gmail.com>
2014-06-19 10:53:54 +0000

    Launch-Ray.sh fixed for renaming reads properly
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- Launch-Ray.sh
+++ Launch-Ray.sh
@@ -10,12 +10,21 @@ projectID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json
 sampleID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json sampleID)
 reads=$(find /data/input/samples/$sampleID -name "*fastq*" | head -n 1)
 readDir=$(dirname $reads)
+for i in $readDir/*
+do
+    gzFile=$(echo $i | grep "fastq" | grep ".gz$")
+    if [ ! -z $gzFile ]
+    then
+	mv $i $i.fastq.gz
+    fi
+done
+
 mkdir Search-Datasets
-mkdir -p /data/output/appresults/$projectID/
+mkdir -p /data/output/appresults/$projectID
 bash /opt/ray-on-basespace/Generate-RayConf.sh -r $readDir -d ./Search-Datasets -k $kmersize -o /data/output/appresults/$projectID/$sampleID
+
 echo "Running Ray Assembly.."
 cat Ray.conf
 mpiexec -n 32 /opt/ray/BUILD/Ray Ray.conf
-ls /data/output/appresults/$projectID/$sampleID
 
 echo "Assembly Finished"
