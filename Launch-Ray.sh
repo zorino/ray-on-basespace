e7f10f11750fc453a8a03f4c3eb7c87195a4c6a2
Maxime Déraspe <maximilien1er@gmail.com>
2014-06-23 11:27:20 +0000

    Launch-Ray.sh fixed : do not take index for assembly
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- Launch-Ray.sh
+++ Launch-Ray.sh
@@ -10,18 +10,19 @@ projectID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json
 sampleID=$(python2 ray-on-basespace/Ray-Launcher.py /data/input/AppSession.json sampleID)
 reads=$(find /data/input/samples/$sampleID -name "*fastq*" | head -n 1)
 readDir=$(dirname $reads)
+mkdir -p /tmp/reads-for-assembly
 for i in $readDir/*
 do
-    gzFile=$(echo $i | grep "fastq" | grep ".gz$")
+    gzFile=$(echo $i | grep "fastq" | grep "_R" | grep ".gz$")
     if [ ! -z $gzFile ]
     then
-	mv $i $i.fastq.gz
+	ln -s $i /tmp/reads-for-assembly/$i.fastq.gz
     fi
 done
 
 mkdir Search-Datasets
 mkdir -p /data/output/appresults/$projectID
-bash /opt/ray-on-basespace/Generate-RayConf.sh -r $readDir -d ./Search-Datasets -k $kmersize -o /data/output/appresults/$projectID/$sampleID
+bash /opt/ray-on-basespace/Generate-RayConf.sh -r /tmp/reads-for-assembly -d ./Search-Datasets -k $kmersize -o /data/output/appresults/$projectID/$sampleID
 
 echo "Running Ray Assembly.."
 cat Ray.conf
